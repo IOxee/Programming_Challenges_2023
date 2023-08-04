1c8c6329837b281467a87f10a121477894f25eb6
IRosell <isidre.rosell@techtiva.tech>
2023-08-04 12:26:57 +0200

    Make challenge Automatic

---
+++ create.bat
@@ -0,0 +1,3 @@
+@echo off
+python makeChallenge.py
+pause
---
+++ makeChallenge.py
@@ -0,0 +1,28 @@
+import os, datetime
+
+def create_challenge_folder():
+    num = input("Ingresa un número para crear la carpeta: ")
+    try:
+        num = int(num)
+    except ValueError:
+        print("Error: Ingresa un número válido.")
+        return
+
+    folder_name = f"challenge-{num}"
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+        with open(os.path.join(folder_name, "solution.py"), 'w') as file:
+            file.write("# Path: solution.py\n# Author: IOxee\n# TimeStamp: " + str(datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S")) + "\n\n")
+            pass
+        
+        challenge_name = input("Ingresa el nombre del reto: ")
+        with open(os.path.join(folder_name, "description.md"), 'w') as file:
+            file.write(f"<center><h1>{challenge_name}</h1></center>")
+            pass
+        
+        print(f"Carpeta '{folder_name}' creada con éxito.")
+    else:
+        print(f"La carpeta '{folder_name}' ya existe.")
+
+if __name__ == "__main__":
+    create_challenge_folder()
